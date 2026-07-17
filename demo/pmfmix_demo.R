656712fd039631a2dbb21427a779263b8a3868c1
David J. H. Shih <djh.shih@gmail.com>
2026-07-17 16:40:57 +0800

    Update demo.

--- demo/pmfmix_demo.R
+++ demo/pmfmix_demo.R
@@ -10,7 +10,7 @@ initialize_theta_beta <- function(C, v, K, hparams) {
   vmax <- max(v)
   mus <- seq(vmin, vmax, length.out = K + 2)[2:(K + 1)]
   lapply(seq_len(K), function(k) {
-    list(mu = mus[k], lambda = 25)
+    list(mu = mus[k], lambda = 1)
   })
 }
 
@@ -51,15 +51,11 @@ update_theta_beta <- function(C, v, params, hparams) {
     # w^T is K by N,  dbeta(x, ...) is K   ->  each item is K by N
     # output is K by N by J; need N by J by K
     lp <- aperm(array(lp, c(K, N, J)), c(2, 3, 1))
-    # message("lp:")
-    # print(str(exp(lp) / sum(exp(lp))))
     lp
   }
 
   # negative log likelihood
   objective <- function(a) {
-    # message("z:")
-    # print(str(params$z / sum(params$z)))
     - sum( params$z * lpdf_transform(a) )
   }
 
@@ -72,47 +68,26 @@ update_theta_beta <- function(C, v, params, hparams) {
   })
 }
 
-# update_theta_beta <- function(C, v, params, hparams) {
-#   Gamma <- params$Gamma;
-#   theta <- params$theta;
-#   K <- dim(Gamma)[3]
-#   lapply(seq_len(K), function(k) {
-#     weights <- colSums(C * Gamma[, , k, drop = FALSE][, , 1])
-#     total <- sum(weights)
-#     if (total <= 0) {
-#       return(theta[[k]])
-#     }
-
-#     mu_hat <- sum(weights * v) / total
-#     var_hat <- sum(weights * (v - mu_hat)^2) / total
-#     lambda_hat <- if (var_hat <= 0) {
-#       hparams$lambda_bounds[2]
-#     } else {
-#       mu_hat * (1 - mu_hat) / var_hat - 1
-#     }
-#     lambda_hat <- max(hparams$lambda_bounds[1], min(hparams$lambda_bounds[2], lambda_hat))
-#     mu_hat <- max(hparams$mu_eps, min(1 - hparams$mu_eps, mu_hat))
-
-#     list(mu = mu_hat, lambda = lambda_hat)
-#   })
-# }
-
-set.seed(2)
-v <- seq(0.05, 0.95, by = 0.1)
+set.seed(1)
+v <- seq(0.05, 0.95, by = 0.05)
 true_theta <- list(
-  list(mu = 0.25, lambda = 18),
+  list(mu = 0.25, lambda = 10),
   list(mu = 0.75, lambda = 30)
 )
 true_w <- rbind(
   c(0.85, 0.15),
   c(0.55, 0.45),
-  c(0.20, 0.80)
+  c(0.20, 0.80),
+  c(0.40, 0.60),
+  c(0.10, 0.90),
+  c(0.90, 0.10)
 )
 
 pmf_mat <- do.call(rbind, lapply(true_theta, function(th) beta_pmf(v, th)))
-counts_total <- 1500
+pmf_mat <- pmf_mat / rowSums(pmf_mat);
 target <- true_w %*% pmf_mat;
-target <- target / rowSums(target);
+rowSums(target)
+counts_total <- 1e5;
 C <- t(apply(target, 1, function(p) as.vector(rmultinom(1, counts_total, p))))
 
 fit <- pmfmix(
@@ -122,11 +97,13 @@ fit <- pmfmix(
   f = beta_pmf,
   initialize_theta = initialize_theta_beta,
   update_theta = update_theta_beta,
-  hparams = list(alpha = c(1, 1), mu_eps = 1e-6, lambda_bounds = c(2, 200)),
-  control = list(nstart = 3, niter = 25, abstol = 1e-6),
+  hparams = list(alpha = c(1, 1)),
+  control = list(nstart = 5, niter = 25, abstol = 1e-6),
   fixed = list(w = NULL, theta = NULL, Gamma = NULL),
   verbose = TRUE
 )
+
+plot(target, C / rowSums(C))
 
 pmf_mat_hat <- t(matrix(unlist(fit$params$f), nrow=ncol(C), ncol=ncol(true_w)));
 plot(pmf_mat, pmf_mat_hat)
@@ -141,11 +118,14 @@ plot(target, target.hat)
 cor(t(target), t(target.hat))
 cor(c(target), c(target.hat))
 
-i <- 3;
-plot(v, target[i, ])
-points(v, target.hat[i, ], col="blue")
+par(mfrow=c(3, 2))
+for (i in 1:nrow(target)) {
+  plot(v, target[i, ], type="l")
+  lines(v, target.hat[i, ], col="blue")
+}
 
 cat("Mixture weights:\n")
 print(round(fit$params$w, 3))
 cat("Component parameters:\n")
 print(lapply(fit$params$theta, function(th) list(mu = round(th$mu, 3), lambda = round(th$lambda, 3))))
+
