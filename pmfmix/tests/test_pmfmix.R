a6adeaa6b916c00f1881ca3f975bf0ee77ef7b4c
David J. H. Shih <djh.shih@gmail.com>
2026-07-22 00:57:57 +0800

    Fix test.

--- pmfmix/tests/test_pmfmix.R
+++ pmfmix/tests/test_pmfmix.R
@@ -6,8 +6,8 @@ beta_lpmf <- function(v, theta) {
 }
 
 initialize_theta_beta <- function(C, v, K, hparams) {
-  mus <- seq(0.2, 0.8, length.out = K)
-  lapply(seq_len(K), function(k) list(mu = mus[k], lambda = 20))
+  mu <- seq(0.2, 0.8, length.out = K)
+  list(mu = mu, lambda = rep(K, 20))
 }
 
 update_theta_identity <- function(C, v, params, hparams) theta
@@ -18,24 +18,20 @@ assert <- function(cond, msg) {
 
 set.seed(42)
 v <- seq(0.05, 0.95, by = 0.1)
-theta <- list(
-  list(mu = 0.3, lambda = 20),
-  list(mu = 0.7, lambda = 25)
-)
+K <- 2;
+theta <- list(mu = c(0.3, 0.7), lambda = c(20, 25));
 true_w <- rbind(c(0.8, 0.2), c(0.25, 0.75))
-pmf_mat <- do.call(rbind, lapply(theta, function(th) exp(beta_lpmf(v, th))))
+pmf_mat <- matrix(unlist(lapply(v, function(x) exp(beta_lpmf(x, theta)))), nrow=K);
 C <- t(apply(true_w %*% pmf_mat, 1, function(p) as.vector(rmultinom(1, 4000, p))))
 
 fit <- pmfmix(
   C = C,
   v = v,
-  K = 2,
+  K = K,
   lf = beta_lpmf,
   initialize_theta = initialize_theta_beta,
   update_theta = update_theta_identity,
-  hparams = list(alpha = c(1, 1)),
   control = list(nstart = 1, niter = 50, abstol = 1e-8),
-  fixed = list(theta = theta, W = NULL, Gamma = NULL),
   verbose = FALSE
 )
 
